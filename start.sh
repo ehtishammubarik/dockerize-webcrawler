a7ed24939f1eef042e6ca14e7b02fe783ff02a9e
Usman Ramzan <usman.ramzan@emumba.com>
2021-07-19 20:17:52 +0500

    Updating start script

--- start.sh
+++ start.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-
-pip3 install scrapyd
-scrapyd > /dev/null 2>&1 &
-scrapyd-deploy
-
-tail -f /dev/null
+pip3 install scrapyd        # To install scrapyd via pip packages
+scrapyd > /dev/null 2>&1 &  # To run the scrapyd in background and redirect the logs to dev/null
+scrapyd-deploy              # For deploying scrapyd
+tail -f /dev/null                       
